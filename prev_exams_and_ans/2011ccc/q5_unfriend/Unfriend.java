50c12989e4dc2242f34003bd11b249d18f020552
yueleng <wangyueleng@gmail.com>
2020-09-30 09:57:15 -0400

    Update Unfriend.java
    * A concise solution to Unfriend

--- prev_exams_and_ans/2011ccc/q5_unfriend/Unfriend.java
+++ prev_exams_and_ans/2011ccc/q5_unfriend/Unfriend.java
@@ -1,3 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Unfriend {
-    
+
+	public static void main(String[] args)  throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int numOfPeople = Integer.parseInt(br.readLine());
+
+		int[] invited = {1, 1, 1, 1, 1, 1, 1};
+		for (int personInvited = 1; personInvited <= numOfPeople-1; personInvited++) {
+			int invitedBy = Integer.parseInt(br.readLine());
+			invited[invitedBy] = invited[invitedBy] + invited[invitedBy] * invited[personInvited];
+		}
+		System.out.println(invited[numOfPeople]);
+	}
+
 }
